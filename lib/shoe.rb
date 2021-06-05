d24f8fe2b809ea77a9e96645621930dc368779a6
Vincent Tran <vincentuong@yahoo.com>
2021-06-05 05:36:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,41 @@
 # Make your shoe class here!
+
+
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def brand
+    @brand
+  end
+ 
+  def author=(author)
+    @author = author
+  end
+ 
+  def author
+    @author
+  end
+ 
+  def page_count=(num)
+    @page_count = num
+  end
+ 
+  def page_count
+    @page_count
+  end
+ 
+  def genre=(genre)
+    @genre = genre
+  end
+ 
+  def genre
+    @genre
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
